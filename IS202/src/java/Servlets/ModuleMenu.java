cecd8dc3148cbe21ea3c009dc5f0510d359c311e
Phuong <phuonghapham@hotmail.cm>
2018-10-23 17:37:43 +0200

    la til klassedeklerasjon

--- IS202/src/java/Servlets/ModuleMenu.java
+++ IS202/src/java/Servlets/ModuleMenu.java
@@ -1,5 +1,13 @@
- * @throws IOException if an I/O error occurs
-     */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ModuleMenu extends HttpServlet{
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
